277210535dbffc2cc78c4141662daaa091521c6b
Timmapuram <shirisha.timmapuram@capgemini.com>
2019-03-22 13:55:34 +0530

    initial commit

---
+++ src/main/java/com/cox/dit/DitApplication.java
@@ -0,0 +1,13 @@
+package com.cox.dit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitApplication.class, args);
+	}
+
+}
